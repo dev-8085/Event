02b87619ceb648e2e78c04e19b094646b844eade
Devang Singh Parihar <parihardevangsingh11@gmail.com>
2025-06-17 17:01:14 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import EventDetailsPage from "./pages/EventDetailsPage";
+import BookingFormPage from "./pages/BookingFormPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-     
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/event/:id" element={<EventDetailsPage />} />
+      <Route path="/book/:id" element={<BookingFormPage />} />
+      <Route path="/confirmation" element={<ConfirmationPage />} />
+    </Routes>
+  );
 }
-
-export default App
